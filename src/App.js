aa699b174ba67b54755c1ac53f7572f60678bda5
Irina Plaksina <irene.plaksina@gmail.com>
2020-11-10 00:07:04 -0500

    charts controls added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import moment from 'moment';
 import Header from './components/header/header';
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Form, InputGroup, Button, ButtonGroup} from "react-bootstrap";
 import React, {Component} from 'react';
 import {Line, Pie} from 'react-chartjs-2';
 
@@ -59,29 +59,63 @@ class App extends Component {
             <>
                 <Header/>
                 <Container className="App">
+                    <Row className='mt-2'>
+                        <Col xs={4}>
+                            <InputGroup size='sm' className='hostSelector shadow'>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text id="hostOptions" className='dark-label'>Host</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <Form.Control as='select' aria-label='Small' aria-describedby='hostOptions'
+                                              className='dark-control'>
+                                    <option>logic-test-01</option>
+                                    <option>logic-test-02</option>
+                                </Form.Control>
+                            </InputGroup>
+                        </Col>
+                        <Col xs={{span: 4, offset: 4}}>
+                            <ButtonGroup size='sm' aria-label="memoryData" className='shadow'>
+                                <Button variant="secondary" className='dark-button'>Active</Button>
+                                <Button variant="secondary" className='dark-button'>MemUsed</Button>
+                                <Button variant="secondary" className='dark-button'>MemCached</Button>
+                                <Button variant="secondary" className='dark-button'>SwapUsed</Button>
+                                <Button variant="secondary" className='dark-button'>SwapCached</Button>
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
+                    <Row className='mt-2'>
+                        <Col xs={4}>
+                            <ButtonGroup size='sm' aria-label="cpuData" className='shadow'>
+                                <Button variant="secondary" className='dark-button'>idle</Button>
+                                <Button variant="secondary" className='dark-button'>user</Button>
+                                <Button variant="secondary" className='dark-button'>system</Button>
+                                <Button variant="secondary" className='dark-button'>iowait</Button>
+                                <Button variant="secondary" className='dark-button'>steal</Button>
+                            </ButtonGroup>
+                        </Col>
+                    </Row>
                     <Row>
                         <Col xs lg={8}>
-                            <Line
-                                data={this.state}
-                                options={{
-                                    title: {
-                                        display: true,
-                                        text: 'Linux CPU',
-                                        fontsize: 20
-                                    },
-                                    legend: {
-                                        display: true,
-                                        position: 'right'
-                                    },
-                                    layout: {
-                                        padding: {
-                                            left: 10,
-                                            right: 10,
-                                            top: 10,
-                                            bottom: 10
-                                        }
-                                    }
-                                }}
+                            <Line className='shadow'
+                                  data={this.state}
+                                  options={{
+                                      title: {
+                                          display: true,
+                                          text: 'Linux CPU',
+                                          fontsize: 20
+                                      },
+                                      legend: {
+                                          display: true,
+                                          position: 'right'
+                                      },
+                                      layout: {
+                                          padding: {
+                                              left: 10,
+                                              right: 10,
+                                              top: 10,
+                                              bottom: 10
+                                          }
+                                      }
+                                  }}
                             />
                         </Col>
                         <Col xs lg={4}>
@@ -107,6 +141,16 @@ class App extends Component {
                                     }
                                 }}
                             />
+                        </Col>
+                    </Row>
+                    <Row className='mt-2'>
+                        <Col xs={4}>
+                            <ButtonGroup size='sm' aria-label="networkData" className='shadow'>
+                                <Button variant="secondary" className='dark-button'>eth0_txbyt</Button>
+                                <Button variant="secondary" className='dark-button'>eth0_txerrs</Button>
+                                <Button variant="secondary" className='dark-button'>eth0_rxbyt</Button>
+                                <Button variant="secondary" className='dark-button'>eth0_rxerrs</Button>
+                            </ButtonGroup>
                         </Col>
                     </Row>
                     <Row>
